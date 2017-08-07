ad992fd9fb596b44b4e395b06fb9681ea37a6c04
James <jordunjames@gmail.com>
2017-08-07 17:14:29 +0100

    initial commit

---
+++ app/src/main/java/com/example/greenmouseacer/myapplication/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.greenmouseacer.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    Button findCategory;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //toolbar.setTitleTextColor(0xFFFFFFFF);
+
+        Spinner spinner = (Spinner) findViewById(R.id.category);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.planets_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
+          findCategory = (Button) findViewById(R.id.find_category);
+        findCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+       getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+
+    }
+
+
+}
